b8b212a9e69c6770d18d3e57231af144efacaa8a
rmangels <mangelschotsroel@hotmail.com>
2013-10-12 23:56:02 +0200

    scroll

--- NetworkPlanningTool/src/com/ugent/networkplanningtool/layout/DesignView.java
+++ NetworkPlanningTool/src/com/ugent/networkplanningtool/layout/DesignView.java
@@ -1,5 +1,7 @@
 package com.ugent.networkplanningtool.layout;
 
+import com.ugent.networkplanningtool.model.FloorModel;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,77 +15,86 @@ public class DesignView extends View {
 
 	private Paint paint = new Paint();
 	
-	private MotionEvent zoomStart;
-	float lines = 10;
+	private FloorModel model;
+	
+	private double distanceStart = 0;
+	private float dragStartX = 0;
+	private float dragStartY = 0;
 
 	public DesignView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		zoomStart = null;
 		setupDesign();
 	}
 
 	private void setupDesign() {
-		// TODO Auto-generated method stub
+		model = new FloorModel();
 
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		
 		paint.setColor(Color.BLACK);
-		int height = getHeight()-1;
-		int width = getWidth()-1;
-		for(int i = 0; i <= lines; i++){
-			canvas.drawLine(0, height*i/lines, width, height*i/lines, paint);
-		}
-		for(int i = 0; i <= lines; i++){
-			canvas.drawLine(width*i/lines, 0, width*i/lines, height, paint);
+		
+		int viewWidth = getWidth();
+		int viewHeight = getHeight();
+		
+		float percentOffsetX = (FloorModel.INTERVAL-(model.getOffsetX() % FloorModel.INTERVAL))/FloorModel.INTERVAL%1;
+		float percentOffsetY = (FloorModel.INTERVAL-(model.getOffsetY() % FloorModel.INTERVAL))/FloorModel.INTERVAL%1;
+		
+		for(float i = percentOffsetX*model.getPixelsPerInterval(); i < viewWidth; i+=model.getPixelsPerInterval()){
+			canvas.drawLine(i, 0f, i, viewHeight, paint);
 		}
 		
+		for(float i = percentOffsetY*model.getPixelsPerInterval(); i < viewWidth; i+=model.getPixelsPerInterval()){
+			canvas.drawLine(0, i, viewWidth, i, paint);
+		}
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		Log.d("DEBUG",""+(event.getAction() & MotionEvent.ACTION_MASK));
-		float touchX = event.getX();
-		float touchY = event.getY();
-
 		switch (event.getAction() & MotionEvent.ACTION_MASK) {
 		case MotionEvent.ACTION_DOWN:
-			Log.d("DEBUG","FINGER 1 DOWN");
 			break;
 
         case MotionEvent.ACTION_MOVE:
-        	if(zoomStart != null){
+        	if(distanceStart > 0){
+        		double distanceMoved = getDistance(event);
         		
+        		model.setPixelsPerInterval(model.getPixelsPerInterval()/(float)(distanceStart/distanceMoved));
+        		distanceStart = distanceMoved;
+        		
+        		model.setOffsetX(dragStartX-Math.min(event.getX(0), event.getX(1))/model.getPixelsPerInterval()*FloorModel.INTERVAL);
+        		model.setOffsetY(dragStartY-Math.min(event.getY(0), event.getY(1))/model.getPixelsPerInterval()*FloorModel.INTERVAL);
+        		
+        		invalidate();
         	}
-        	Log.d("DEBUG","FINGER 1 MOVE");
             break;
-
         case MotionEvent.ACTION_UP:
-        	Log.d("DEBUG","FINGER 1 UP");
+        	
             //first finger went up
             break;
-
         case MotionEvent.ACTION_CANCEL:
-        	Log.d("DEBUG","FINGER 1 CANCEL");
             break;
-
         case MotionEvent.ACTION_POINTER_DOWN:
-        	zoomStart = event;
-        	Log.d("DEBUG","FINGER 2 DOWN");
-            //second finger (or third, or more) went down.
+        	distanceStart = getDistance(event);
+        	dragStartX = model.getOffsetX()+Math.min(event.getX(0), event.getX(1))/model.getPixelsPerInterval()*FloorModel.INTERVAL;
+        	Log.d("PEACOCK",""+dragStartX);
+        	dragStartY = model.getOffsetY()+Math.min(event.getY(0), event.getY(1))/model.getPixelsPerInterval()*FloorModel.INTERVAL;
             break;
-
         case MotionEvent.ACTION_POINTER_UP:
-        	zoomStart = null;
-            //second finger (or more) went up.
-        	Log.d("DEBUG","FINGER 2 UP");
+        	distanceStart = 0;
             break;
-
         default: break;
 		}
 		return true;
+	}
+
+	private double getDistance(MotionEvent event) {
+		double distance1 = Math.sqrt(Math.pow(event.getX(1)-event.getX(0), 2)+
+				Math.pow(event.getY(1)-event.getY(0), 2));
+		return distance1;
 	}
 
 
--- NetworkPlanningTool/src/com/ugent/networkplanningtool/model/FloorModel.java
+++ NetworkPlanningTool/src/com/ugent/networkplanningtool/model/FloorModel.java
@@ -1,5 +1,51 @@
 package com.ugent.networkplanningtool.model;
 
 public class FloorModel {
-	private float scaleFactor;
+	
+	public static final float INTERVAL = 0.5f;
+	public static final float FLOOR_WIDTH = 100f;
+	public static final float FLOOR_HEIGHT = 75f;
+	
+	private float offsetX;
+	private float offsetY;
+	private float pixelsPerInterval;
+	
+	
+	public FloorModel(){
+		offsetX = 0;
+		offsetY = 0;
+		pixelsPerInterval = 100;
+	}
+
+
+	public float getOffsetX() {
+		return offsetX;
+	}
+
+
+	public void setOffsetX(float offsetX) {
+		this.offsetX = offsetX<0?0:offsetX;
+	}
+
+
+	public float getOffsetY() {
+		return offsetY;
+	}
+
+
+	public void setOffsetY(float offsetY) {
+		this.offsetY = offsetY<0?0:offsetY;
+	}
+
+
+	public float getPixelsPerInterval() {
+		return pixelsPerInterval;
+	}
+
+
+	public void setPixelsPerInterval(float pixelsPerInterval) {
+		this.pixelsPerInterval = pixelsPerInterval < 30 ? 30 : pixelsPerInterval;
+	}
+	
+	
 }
